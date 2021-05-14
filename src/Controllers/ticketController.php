318aad484b9a4b53ddc5b6d148920dce1341d4a0
gbuisson <guillaume.buisson.simplon@gmail.com>
2021-05-14 15:21:38 +0200

    add endpoints

--- src/Controllers/ticketController.php
+++ src/Controllers/ticketController.php
@@ -17,6 +17,20 @@ switch($request_method)
             getTickets();
         }
         break;
+    case 'PUT':
+        // Modifier un produit
+        $id = intval($_GET["id"]);
+        updateTicket($id);
+        break;
+    case 'POST':
+        // Ajouter un produit
+        addTicket();
+        break;
+    case 'DELETE':
+        // Supprimer un produit
+        $id = intval($_GET["id"]);
+        deleteTicket($id);
+        break;
     default:
         // Requête invalide
         header("HTTP/1.0 405 Method Not Allowed");
@@ -53,4 +67,85 @@ function getTicket($id=0)
     }
     header('Content-Type: application/json');
     echo json_encode($response, JSON_PRETTY_PRINT);
+}
+
+function addTicket()
+{
+    global $conn;
+    $title = $_POST["title"];
+    $body = $_POST["body"];
+    $actualPosition = $_POST["actualPosition"];
+    $status = $_POST["status"];
+    $color = $_POST["color"];
+    echo $query="INSERT INTO postit(title, body, ActualPosition, status, color) VALUES('".$title."', '".$body."', '".$actualPosition."', '".$status."', '".$color."')";
+    if(mysqli_query($conn, $query))
+    {
+        $response=array(
+            'status' => 1,
+            'status_message' =>'Postit ajout� avec succ�s.'
+        );
+    }
+    else
+    {
+        $response=array(
+            'status' => 0,
+            'status_message' =>'ERREUR!.'. mysqli_error($conn)
+        );
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
+
+}
+
+function updateTicket($id=0) {
+    global $conn;
+    $_PUT = array();
+    parse_str(file_get_contents('php://input'), $_PUT);
+    $title = $_PUT["title"];
+    $body = $_PUT["body"];
+    $actualPosition = $_PUT["actualPosition"];
+    $status = $_PUT["status"];
+    $color = $_PUT["color"];
+    $query="UPDATE postit SET title='".$title."', body='".$body."', actualPosition='".$actualPosition."', status='".$status."', color='".$color."' WHERE id=".$id;
+
+    if(mysqli_query($conn, $query))
+    {
+        $response=array(
+            'status' => 1,
+            'status_message' =>'Postit mis a jour avec succes.'
+        );
+    }
+    else
+    {
+        $response=array(
+            'status' => 0,
+            'status_message' =>'Echec de la mise a jour du postit. '. mysqli_error($conn)
+        );
+
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+function deleteTicket($id)
+{
+    global $conn;
+    $query = "DELETE FROM postit WHERE id=".$id;
+    if(mysqli_query($conn, $query))
+    {
+        $response=array(
+            'status' => 1,
+            'status_message' =>'Produit supprime avec succes.'
+        );
+    }
+    else
+    {
+        $response=array(
+            'status' => 0,
+            'status_message' =>'La suppression du produit a echoue. '. mysqli_error($conn)
+        );
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
 }
